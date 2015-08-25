596d6914c7efb4b998aa09095e9b357ee236765f
Roi <roi.bueno@gmail.com>
2015-08-25 14:29:03 +0300

    just for backup

--- domFromDB.php
+++ domFromDB.php
@@ -1,210 +1,299 @@
-<style>
-.hidden{
-	display: none !important;
-}
-.mandatory{
-	box-shadow: 4px 4px 20px rgba(200, 0, 0, 0.85);
-	padding: 10px;
-}
-.mandatory:focus {
-  border: 1px solid red;
-  outline: none;
-}
-.mandatory:hover {
-  opacity: 1;
-}
-body{
-	direction: rtl;
-}
-.centerred{
-	margin-right: auto;
-    margin-left: auto;
-    font-family: cursive;
-    font-size: 18px;
-    font-weight: bold;
-}
-/* Elegant Aero */
-.elegant-aero {
-    margin-left:auto;
-    margin-right:auto;
-
-    max-width: 500px;
-    background: #D2E9FF;
-    padding: 20px 20px 20px 20px;
-    font: 12px Arial, Helvetica, sans-serif;
-    color: #666;
-}
-.elegant-aero h1 {
-    font: 24px "Trebuchet MS", Arial, Helvetica, sans-serif;
-    padding: 10px 10px 10px 20px;
-    display: block;
-    background: #C0E1FF;
-    border-bottom: 1px solid #B8DDFF;
-    margin: -20px -20px 15px;
-}
-.elegant-aero h1>span {
-    display: block;
-    font-size: 11px;
-}
-
-.elegant-aero label>span {
-    float: left;
-    margin-top: 10px;
-    color: #5E5E5E;
-}
-.elegant-aero label {
-    display: block;
-    margin: 0px 0px 5px;
-}
-.elegant-aero label>span {
-    float: left;
-    width: 20%;
-    text-align: right;
-    padding-right: 15px;
-    margin-top: 10px;
-    font-weight: bold;
-}
-.elegant-aero input[type="text"], .elegant-aero input[type="email"], .elegant-aero textarea, .elegant-aero select {
-    color: #888;
-    width: 70%;
-    padding: 0px 0px 0px 5px;
-    border: 1px solid #C5E2FF;
-    background: #FBFBFB;
-    outline: 0;
-    -webkit-box-shadow:inset 0px 1px 6px #ECF3F5;
-    box-shadow: inset 0px 1px 6px #ECF3F5;
-    font: 200 12px/25px Arial, Helvetica, sans-serif;
-    height: 30px;
-    line-height:15px;
-    margin: 2px 6px 16px 0px;
-}
-.elegant-aero textarea{
-    height:100px;
-    padding: 5px 0px 0px 5px;
-    width: 70%;
-}
-.elegant-aero select {
-    background: #fbfbfb url('down-arrow.png') no-repeat right;
-    background: #fbfbfb url('down-arrow.png') no-repeat right;
-   appearance:none;
-    -webkit-appearance:none; 
-   -moz-appearance: none;
-    text-indent: 0.01px;
-    text-overflow: '';
-    width: 70%;
-}
-.elegant-aero .button{
-    padding: 10px 30px 10px 30px;
-    background: #66C1E4;
-    border: none;
-    color: #FFF;
-    box-shadow: 1px 1px 1px #4C6E91;
-    -webkit-box-shadow: 1px 1px 1px #4C6E91;
-    -moz-box-shadow: 1px 1px 1px #4C6E91;
-    text-shadow: 1px 1px 1px #5079A3;
-    
-}
-.elegant-aero .button:hover{
-    background: #3EB1DD;
-}
-</style>
+<link rel="stylesheet" href="css/style.css" type="text/css">
 <?php $intid=isset($_GET["intid"])?$_GET["intid"]:"" ?>
 <?php
-class TableRows extends RecursiveIteratorIterator { 
-    function __construct($it) { 
-        parent::__construct($it, self::LEAVES_ONLY); 
+
+class Action { 
+
+	/* Credentials */
+	public $servername = "localhost";
+	public $username = "root";
+	public $password = "";
+	public $dbname = "bmby";
+	public $generalTable = "mbeat_form";
+	public $fieldsTable = "mbeat_form_fields";
+	
+	/*Structure Functions*/
+	function __construct( ){  }	
+	function before() {
+		return new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	}
+	function after() {
+		return $conn = null;
+	}
+
+	/*External DB Functions*/
+	function getUsersForDropdown() {
+		/*SHADI COMPLETES*/
+	}
+	function getContentForFieldsDropdown() {
+		/*SHADI COMPLETES*/
+	}
+	
+	/*API Functions*/
+	/*-------------*/
+	/*Form Manipulations*/    
+    function addNewForm($conn, $intid) {
+	    try {
+			    // set the PDO error mode to exception
+			    $sql = "INSERT INTO $this->generalTable (intid) VALUES (:intid)";
+							    // use exec() because no results are returned
+				 $conn->exec($sql);
+			}
+			catch(PDOException $e) {
+			    echo $sql . "<br>" . $e->getMessage();
+			}
+	}
+	function showAllForms($conn) {
+		$stmt = $conn->prepare("SELECT intid from $this->generalTable");
+		$stmt->execute();
+		foreach (new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v){
+			$this->retrieveFormData($conn, $v["intid"]);
+		}
+	}
+	function retrieveFormData($conn, $intid) {
+	try {	
+			$ft = $this->fieldsTable;
+			$gt = $this->generalTable;
+		    $stmt = $conn->prepare("SELECT $ft.intid, $ft.fieldType, $ft.fieldLabel, $ft.fieldname, $ft.optionalVals, $ft.isMandatory, 
+		    	$ft.isHidden, $ft.validOptions, $ft.dateUpdate, $ft.userUpdate, $gt.greetingsUrl 
+		    	FROM $ft
+		    	JOIN $gt
+		    	ON $ft.intid = $gt.intid 
+		    	WHERE $ft.intid=$intid");
+		    $stmt->execute();
+			foreach(new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v) {
+		   		print_r($v);
+		   	} 
+		}
+		catch(PDOException $e) {
+		    echo "Error: " . $e->getMessage();
+		}
+	}
+	
+	/*Update&Save Screens*/
+    function updateFormScreen1($conn, $intid, $landingPageName, $landingPageUrl, $media, $handleReLid) { 
+	    try {
+		    // set the PDO error mode to exception
+		    $sql = "UPDATE $this->generalTable SET landingPageName=:landingPageName, landingPageUrl=:landingPageUrl, media:=media, handleReLid=:handleReLid WHERE intid=:intid";
+		    // Prepare statement
+		    $stmt = $conn->prepare($sql);
+		    // execute the query
+		    $stmt->execute();
+		    }
+		catch(PDOException $e)
+		    {
+		    echo $sql . "<br>" . $e->getMessage();
+		    }
+    } 	
+    function updateFormScreen2($conn, $intid, $buttonText, $sms, $mail, $deliveryText, $linkToCondTerms, $nameOfCondTerms) {
+    	    try {
+		    // set the PDO error mode to exception
+		    $sql = "UPDATE $this->generalTable SET buttonText=:buttonText, sms=:sms, mail:=mail, deliveryText=:deliveryText, linkToCondTerms=:linkToCondTerms, nameOfCondTerms=:nameOfCondTerms WHERE intid=:intid";
+		    // Prepare statement
+		    $stmt = $conn->prepare($sql);
+		    // execute the query
+		    $stmt->execute();
+		    }
+		catch(PDOException $e)
+		    {
+		    echo $sql . "<br>" . $e->getMessage();
+		    }
+    }   
+    function updateFormScreen3($RTL, $LOP, $buttonFont, $buttonFontColor, $buttonColor, $buttonImgId, $formWidth, $formBG, $formBorderWidth, $formFrameColor, $fontSize, $fontColor, $fieldWidth, $fieldBG, $fieldBorderWidth, $fieldFrameColor, $fieldFormSize, $fieldFormColor, $cssInput) {
+        try {
+		    // set the PDO error mode to exception
+		    $sql = "UPDATE $this->generalTable SET RTL=:RTL, LOP=:LOP, buttonFont:=buttonFont, buttonFontColor=:buttonFontColor, buttonColor=:buttonColor, buttonImgId=:buttonImgId, formWidth=:formWidth, formBG=:formBG, formBorderWidth=:formBorderWidth, formFrameColor=:formFrameColor, fontSize=:fontSize, fontColor=:fontColor, fieldWidth=:fieldWidth, fieldBG=:fieldBG, fieldBorderWidth=:fieldBorderWidth, fieldFrameColor=:fieldFrameColor, fieldFormSize=:fieldFormSize, fieldFormColor=:fieldFormColor, cssInput=:cssInput WHERE intid=:intid";
+		    // Prepare statement
+		    $stmt = $conn->prepare($sql);
+		    // execute the query
+		    $stmt->execute();
+		    }
+		catch(PDOException $e)
+		    {
+		    echo $sql . "<br>" . $e->getMessage();
+		    }
+    }    
+    function updateFormScreen4($campaignName, $adsGroup, $creativeName, $refsMedia, $refsAddOns, $refsLinks) {
+        try {
+		    // set the PDO error mode to exception
+		    $sql = "UPDATE $this->generalTable SET campaignName=:campaignName, adsGroup=:adsGroup, creativeName:=creativeName, refsMedia=:refsMedia, refsAddOns=:refsAddOns, refsLinks=:refsLinks WHERE intid=:intid";
+		    // Prepare statement
+		    $stmt = $conn->prepare($sql);
+		    // execute the query
+		    $stmt->execute();
+		    }
+		catch(PDOException $e)
+		    {
+		    echo $sql . "<br>" . $e->getMessage();
+		    }
+    }    
+    function updateFieldsValues($conn, $id, $intid, $fieldType, $fieldLabel, $fieldname, $optionalVals, $isMandatory, $isHidden, $validOptions) {
+    	try {
+    		// set the PDO error mode to exception
+    		$sql = "UPDATE $this->fieldsTable SET intid=:intid, fieldType:=fieldType, fieldLabel=:fieldLabel, fieldname=:fieldname, optionalVals=:optionalVals, isMandatory=:isMandatory, isHidden=:isHidden, validOptions=:validOptions WHERE id=:id";
+    		// Prepare statement
+    		$stmt = $conn->prepare($sql);
+    		// execute the query
+    		$stmt->execute();
+    	}
+    	catch(PDOException $e)
+    	{
+    		echo $sql . "<br>" . $e->getMessage();
+    	}
     }
 
-    function current() {
-        return parent::current();
-    }
+    /*Generation Functions*/
+    function generateFormJSScript($conn, $intid) {
+    	//echo '<script type="text/javascript">';
+		echo "if (str.length==0) { 
+				    document.getElementById('currentForm').innerHTML='';
+				    return;
+				} else {
+				    var xmlhttp=new XMLHttpRequest();
+				    xmlhttp.onreadystatechange=function() {
+				        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+				            document.getElementById('currentForm').innerHTML=xmlhttp.responseText;
+				        }
+				    }
+				    xmlhttp.open('GET','domFromDB.php?intid='$intid,true);
+				    xmlhttp.send();
+				}";
 
-    function beginChildren() { 
-        //echo "<tr>"; 
-    } 
+    }	
+	function generateFormHTMLScript($conn, $intid){
+		try {
+		    $stmt = $conn->prepare("SELECT intid, fieldType, fieldLabel, fieldname, optionalVals, isMandatory, isHidden, validOptions, dateUpdate, userUpdate FROM $this->fieldsTable WHERE intid=$intid");
+		    $stmt->execute();
+			$allVals = "";
+			/*Special Values*/
+			//greetingsAddress
+			$greetingsAddress = $conn->prepare("SELECT greetingsUrl FROM $this->generalTable WHERE intid=$intid");
+			$greetingsAddress->execute();
+			$gAddCol = $greetingsAddress->fetchColumn();
+			//CSS Code
+			$cssCode = $conn->prepare("SELECT cssInput FROM $this->generalTable WHERE intid=$intid");
+			$cssCode->execute();
+			$cssToInject = $cssCode->fetchColumn();
+			//Button Text
+			$bText = $conn->prepare("SELECT buttonText FROM $this->generalTable WHERE intid=$intid");
+			$bText->execute();
+			$bt = $bText->fetchColumn();
+			//Condition Terms - Name
+			$ctn = $conn->prepare("SELECT nameOfCondTerms FROM $this->generalTable WHERE intid=$intid");
+			$ctn->execute();
+			$ctName = $ctn->fetchColumn();
+			//Condition Terms - Link
+			$ctl = $conn->prepare("SELECT linkToCondTerms FROM $this->generalTable WHERE intid=$intid");
+			$ctl->execute();
+			$ctLink = $ctl->fetchColumn();
+			//Delivery Text
+			$dt = $conn->prepare("SELECT deliveryText FROM $this->generalTable WHERE intid=$intid");
+			$dt->execute();
+			$dText = $dt->fetchColumn();
+			//RTL
+			$rtl = $conn->prepare("SELECT RTL FROM $this->generalTable WHERE intid=$intid");
+			$rtl->execute();
+			$formDirection = $rtl->fetchColumn();
+			if($formDirection == 'rtl')
+				$formCSSCode = "body {direction: rtl;}";
+			else $formCSSCode = "body {direction: ltr;}";
+			//LOP
+			$lop = $conn->prepare("SELECT LOP FROM $this->generalTable WHERE intid=$intid");
+			$lop->execute();
+			$orientation = $lop->fetchColumn();
+			if($orientation == 'land')
+				$formCSSCode .= "";//complete
+			else $formCSSCode .= "";//complete
+			//Button Font
+			$bf = $conn->prepare("SELECT buttonFont FROM $this->generalTable WHERE intid=$intid");
+			$bf->execute();
+			$bFont = $bf->fetchColumn();
+			$formCSSCode .= ".button {font-family: ".$bFont.";}";
+			//Button Font Color
+			$bfc = $conn->prepare("SELECT buttonFontColor FROM $this->generalTable WHERE intid=$intid");
+			$bfc->execute();
+			$bFontColor = $bfc->fetchColumn();
+			if($bFontColor == 'trans')
+				$formCSSCode .= ".button {color: transparent;}";
+			else	
+				$formCSSCode .= ".button {color: ".$bFontColor.";}";
+			//Apply CSS on Form
+			echo "<style>".$formCSSCode."</style>";
+			print_r($formCSSCode);
+		    // set the resulting array to associative
+		    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+		    echo '<form action="'.$gAddCol.'" method="post" class="STYLE-NAME"><div class="centerred elegant-aero"><h1>טופס לדוגמא<span>אנא הזן את פרטיך:</span></h1>';
+		    foreach(new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v) {
+		    		$optValsStr = $v["optionalVals"];
+		    		$optArray = explode('|', $optValsStr);
+		    		$currClasses = $this->fieldChecker($v["isMandatory"], $v["isHidden"]);
+		    		echo '<label class="fieldLabel '.$currClasses.'">'.$v["fieldLabel"].' ';
+		   			switch ($v["fieldType"]) {
+		   				case "textarea":
+		   					echo '<div class="textarea"><textarea></textarea></div><br><br>';
+		   					break;
+		   				case "input":
+		   					$currClasses = $this->fieldChecker($v["isMandatory"], $v["isHidden"]);
+		   					echo '<input class="input"><br><br>';
+		   					break;
+		   				case "radio":
+		   					$currClasses = $this->fieldChecker($v["isMandatory"], $v["isHidden"]);
+		   					foreach($optArray as $value)
+		   					{
+		   						echo '<input type="radio" name="radioOptions" value="'.$value.'">'.$value;
+		   					}
+		   					echo '<br><br>';
+		   					break;
+		   				case "checkbox":
+		   					$currClasses = $this->fieldChecker($v["isMandatory"], $v["isHidden"]);
+							foreach($optArray as $value)
+							{
+							    echo '<input type="checkbox" name="checkboxOptions" value="'.$value.'">'.$value;
+							}
+							echo '<br><br>';
+		   					break;
+		   				case "dropdown":
+		   					$currClasses = $this->fieldChecker($v["isMandatory"], $v["isHidden"]);
+		   					echo '<select class="dropdown">';
+		    				foreach($optArray as $value)
+		    				{
+		    					echo '<option value="'.$value.'">'.$value.'</option>';
+		    				}
+		    				echo '</select><br><br>';
+		   					break;
+		   				default:
+		   					echo "";
+		   			} 
+					echo "</label><br>";
+		    }
+		}
+		catch(PDOException $e) {
+		    echo "Error: " . $e->getMessage();
+		}
+		echo '<br><br><label><span>&nbsp;</span><input type="submit" class="button" value="'.$bt.'" /></label>';
+		echo '<br><br><a href="www.google.co.il">'.$ctName.'</a>';
+		echo '<br><br><label><span>&nbsp;</span>'.$dText.'</label>';
+		echo '</div></form>';
+		}	
+	
+	/*Validation Functions*/
+	function fieldChecker($mandatory, $hidden) {
+			$finalClasses = "";
+			if ($mandatory == 1)
+				$finalClasses = "mandatory ";
+			if ($hidden == 1)
+				$finalClasses .= "hidden";
+			return $finalClasses;
+		}
 
-    function endChildren() { 
-        //echo "</tr>" . "\n";
-    } 
-} 
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bmby";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $stmt = $conn->prepare("SELECT mbeat_form.intid, fieldType, fieldLabel, fieldname, optionalVals, isMandatory, isHidden FROM mbeat_form WHERE intid=$intid"); 
-    $stmt->execute();
-	$allVals = "";
-	$greetingsAddress = $conn->prepare("SELECT greetingsUrl FROM mbeat_generalform WHERE intid=$intid");
-	$greetingsAddress->execute();
-	$gAddCol = $greetingsAddress->fetchColumn();
-	$cssCode = $conn->prepare("SELECT cssInput FROM mbeat_generalform WHERE intid=$intid");
-	$cssCode->execute();
-	$cssToInject = $cssCode->fetchColumn();
-	echo "<style>".$cssToInject."</style>";
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    echo '<form action="'.$gAddCol.'" method="post" class="STYLE-NAME"><div class="centerred elegant-aero"><h1>טופס לדוגמא<span>אנא הזן את פרטיך:</span></h1>';
-    foreach(new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v) {
-    		$optValsStr = $v["optionalVals"];
-    		$optArray = explode('|', $optValsStr);
-    		$currClasses = fieldChecker($v["isMandatory"], $v["isHidden"]);
-    		echo '<label class="fieldLabel '.$currClasses.'">'.$v["fieldLabel"].' ';
-   			switch ($v["fieldType"]) {
-   				case "textarea":
-   					echo '<div class="textarea"><textarea></textarea></div><br><br>';
-   					break;
-   				case "input":
-   					$currClasses = fieldChecker($v["isMandatory"], $v["isHidden"]);
-   					echo '<input class="input"><br><br>';
-   					break;
-   				case "radio":
-   					$currClasses = fieldChecker($v["isMandatory"], $v["isHidden"]);
-   					foreach($optArray as $value)
-   					{
-   						echo '<input type="radio" name="radioOptions" value="'.$value.'">'.$value;
-   					}
-   					echo '<br><br>';
-   					break;
-   				case "checkbox":
-   					$currClasses = fieldChecker($v["isMandatory"], $v["isHidden"]);
-					foreach($optArray as $value)
-					{
-					    echo '<input type="checkbox" name="checkboxOptions" value="'.$value.'">'.$value;
-					}
-					echo '<br><br>';
-   					break;
-   				case "dropdown":
-   					$currClasses = fieldChecker($v["isMandatory"], $v["isHidden"]);
-   					echo '<select class="dropdown">';
-    				foreach($optArray as $value)
-    				{
-    					echo '<option value="'.$value.'">'.$value.'</option>';
-    				}
-    				echo '</select><br><br>';
-   					break;
-   				default:
-   					echo "";
-   			} 
-			echo "</label><br>";
-    }
 }
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-echo '<br><br><label><span>&nbsp;</span><input type="submit" class="button" value="שלח" /></label></div></form>';
-$conn = null;
 
-function fieldChecker($mandatory, $hidden) {
-	$finalClasses = "";
-	if ($mandatory == 1)
-		$finalClasses = "mandatory ";
-	if ($hidden == 1)
-		$finalClasses .= "hidden";
-	return $finalClasses;
-}
+
+/*MAIN*/
+$mainAction = new Action();
+$conn = $mainAction->before();
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$mainAction->generateFormHTMLScript($conn, $intid);
+$conn = $mainAction->after();
 
 ?>
